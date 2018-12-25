6d2c3043c3568e2aa9d7822f03f26903762269d7
mayur10 <mayurb609@gmail.com>
2018-12-25 12:35:12 +0530

    Update pwabuilder-sw.js
    changing route of index.html and offline.html

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -6,7 +6,7 @@ var preLoad = function(){
   console.log('[PWA Builder] Install Event processing');
   return caches.open('pwabuilder-offline').then(function(cache) {
     console.log('[PWA Builder] Cached index and offline page during Install');
-    return cache.addAll(['/offline.html', '/index.html']);
+    return cache.addAll(['Refer-A-Friend/offline.html', 'Refer-A-Friend/index.html']);
   });
 }
 
