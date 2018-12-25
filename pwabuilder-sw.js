33e60a44e8cf33c4b60f99be2dab66b4451c9e83
mayur10 <mayurb609@gmail.com>
2018-12-25 11:50:56 +0530

    Update pwabuilder-sw.js
    removing comments.

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,6 +1,3 @@
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
-//Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', function(event) {
   event.waitUntil(preLoad());
 });
