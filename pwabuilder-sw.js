f53d1f23b7e667a27f63c677430df8199628b25a
mayur10 <mayurb609@gmail.com>
2018-12-28 22:45:01 +0530

    Update pwabuilder-sw.js
    adding add to homescreen button.

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -10,6 +10,30 @@ var preLoad = function(){
     return cache.addAll(['./offline.html', './index.html']);
   });
 }
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+});
+btnAdd.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  btnAdd.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
 
 self.addEventListener('fetch', function(event) {
   console.log('[PWA Builder] The service worker is serving the asset.');
